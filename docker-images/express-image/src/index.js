78200f4db235304ae52137dedef053cf4663baa1
Basler Corentin <corentin.basler@heig-vd.ch>
2018-06-09 16:47:19 +0200

    Step 2 done

---
+++ docker-images/express-image/src/index.js
@@ -0,0 +1,38 @@
+var Chance = require('chance');
+var chance = new Chance();
+ 
+var express = require('express');
+var app = express();
+ 
+app.get('/', function(req, res){
+	 res.send(generateAnimals());
+});
+
+app.listen(3000, function(){
+	console.log('Accepting HTTP request on port 3000.');
+});
+
+function generateAnimals(){
+	var animalTypes = new Array("ocean", "desert", "grassland", "forest", "farm", "pet", "zoo");
+	var numberOfAnimals = chance.integer({
+		min: 0,
+		max: 15
+	});
+	console.log(numberOfAnimals);
+	var animalType = chance.integer({
+		min: 0,
+		max: 6
+	});
+	var animals = [];
+	for(var i = 0; i < numberOfAnimals; i++){
+		animals.push({
+			animal: chance.animal({
+				type: animalTypes[animalType]
+			})
+		});
+	};
+	console.log(animals);
+	return animals; 
+}
+
+
